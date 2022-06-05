d180ebf7be11c0f493c8eeda2836b3f63f19a877
Annu <annusinghraj1998@gmail.com>
2022-06-05 20:30:41 +0530

    ADDED : Starting a game

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -5,6 +5,13 @@ public class GamblingSimulator {
     public static void main(String[] args) {
 
         System.out.println("Welcome to the Gambling Problem !");
+        System.out.println("...................");
+
+        int stake_Amount = 100;
+        int bet = 1;
+
+        System.out.println("Amount of stake to start game  of every day is " + stake_Amount);
+        System.out.println("Amount to bet after every game " + bet);
 
 
     }
