9adeae4678b0473541144d8d33227fdd175cace7
Annu <annusinghraj1998@gmail.com>
2022-06-07 07:53:03 +0530

    ADDED : Find out luckiest day and unluckiest day

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -1,38 +1,68 @@
 package com.bridgelabz;
 
 public class GamblingSimulator {
+    static int total_Stake_Amount = 100;
+    final static int BET_AMOUNT = 1;
+    static int winDays = 0;
+    static int lossDays = 0;
+    static int win = 0;
+    static int loss = 0;
+    static int winMax = 0;
+    static int lossMax = 0;
+    static int luckyDay = 0;
+    static int unluckyDay = 0;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator");
-        int total_Amount = 100;
-        int bet_Amount = 1;
-        while (total_Amount > 50 && total_Amount < 150) {
-            int gameAmount = (int) Math.floor(Math.random() * 10) % 2;
 
-            System.out.println("Gambler received amount : " + gameAmount);
+        int winning = 0;
+        int stake_Upper = 150;
+        int stake_Lower = 50;
 
-            if (bet_Amount == gameAmount) {
-                System.out.println("Gambler won the current game");
-                total_Amount = total_Amount + bet_Amount;
-                System.out.println("Total Current case of gambler has : " + total_Amount);
+        for (int days = 1; days <= 30; days++) {
+            total_Stake_Amount = 100;
 
-            } else {
-                System.out.println("Gambler lost the  current game");
-                total_Amount = total_Amount - bet_Amount;
-                System.out.println("Total Current case of gambler has : " + total_Amount);
+            while (total_Stake_Amount > stake_Lower && total_Stake_Amount < stake_Upper) {
+                int gameAmount = (int) (Math.floor(Math.random() * 10)) % 2;
 
+                //System.out.println("Gambler received amount : " + gameAmount);
+
+                if (BET_AMOUNT == gameAmount) {
+                    // System.out.println("Gambler won the current game");
+                    total_Stake_Amount = total_Stake_Amount + BET_AMOUNT;
+                    win++;
+                    // System.out.println("Total Current cash of gambler has : " + total_Stake_Amount);
+
+                } else {
+                    //System.out.println("Gambler lost the  current game");
+                    total_Stake_Amount = total_Stake_Amount - BET_AMOUNT;
+                    loss++;
+                    //System.out.println("Total Current cash of gambler has : " + total_Stake_Amount);
+                }
             }
+            if (total_Stake_Amount == stake_Upper) {
+                winDays += 1;
+                winning += 50;
+            } else {
+                lossDays += 1;
+                winning -= 50;
+            }
+            if (win > winMax) {
+                winMax = win;
+                luckyDay = days + 1;
+            }
+            if (loss > lossMax) {
+                lossMax = loss;
+                unluckyDay = days + 1;
+            }
+            win = 0;
+            loss = 0;
         }
-        System.out.println("Total amount received by gambler has :" + total_Amount);
-
-        if (total_Amount == 50 || total_Amount == 150) {
-            System.out.println(".....................");
-            System.out.println("Player would like to resign for current day !");
-            System.out.println("....................");
-        }
-
-
+        System.out.println("The Luckiest day is : " + "Day " + luckyDay);
+        System.out.println("The Unluckiest day is : " + "Day " + unluckyDay);
+        System.out.println("Total win days : " + winDays);
+        System.out.println("Total loss days : " + lossDays);
+        System.out.println("Total win price won in month is : " + winning + "\n");
     }
-
 
 }
